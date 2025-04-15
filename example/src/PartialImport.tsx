0214da6fb120ef872b9361c917605241f87dbe6f
nagayama <nagayama@iroirodesign.com>
2025-04-15 15:00:13 +0900

    update example

--- example/src/PartialImport.tsx
+++ example/src/PartialImport.tsx
@@ -1,4 +1,4 @@
-import { IconHomeOutlined, IconHomeFilled } from "@serendie/symbol";
+import { SerendieSymbolHome, SerendieSymbolHomeFilled } from "@serendie/symbol";
 
 export function PartialImport() {
   return (
@@ -8,30 +8,30 @@ export function PartialImport() {
       <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
         <div>
           <h3>Outlined</h3>
-          <IconHomeOutlined />
+          <SerendieSymbolHome />
         </div>
 
         <div>
           <h3>Filled</h3>
-          <IconHomeFilled />
+          <SerendieSymbolHomeFilled />
         </div>
       </div>
 
       <div style={{ marginTop: "2rem" }}>
         <h3>Different Sizes</h3>
         <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
-          <IconHomeOutlined width={16} height={16} />
-          <IconHomeOutlined width={24} height={24} />
-          <IconHomeOutlined width={32} height={32} color="red" />
+          <SerendieSymbolHome width={16} height={16} />
+          <SerendieSymbolHome width={24} height={24} />
+          <SerendieSymbolHome width={32} height={32} color="red" />
         </div>
       </div>
 
       <div style={{ marginTop: "2rem" }}>
         <h3>Different Colors</h3>
         <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
-          <IconHomeOutlined color="red" />
-          <IconHomeFilled color="blue" />
-          <IconHomeFilled color="green" />
+          <SerendieSymbolHome color="red" />
+          <SerendieSymbolHomeFilled color="blue" />
+          <SerendieSymbolHomeFilled color="green" />
         </div>
       </div>
     </div>
